166fe207d8484b311ca27fd5fa7669d26479ebfa
Qasem <jaberqasem75@gmail.com>
2023-02-07 11:31:06 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tic_tac_toegame/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tic_tac_toegame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
